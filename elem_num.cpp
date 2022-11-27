7e62e3cc022f0f9c078f926c7b5e04b690e6fb78
mparsyan <martin.parsyan@protonmail.com>
2022-11-27 23:31:47 +0400

    Elements that have numbers

---
+++ elem_num.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::string> elem_num(std::vector<std::string> v)
+{
+    bool flag = false;
+    std::vector<std::string> v1;
+    for (int i = 0; i < v.size(); i++) // <"Hello" "1student" "bestfrends4ever" "bye">
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            if (isdigit(v[i][j]) == true)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (flag)
+        {
+            v1.push_back(v[i]);
+        }
+        flag = false;
+    }
+    return v1;
+}
+
+int main()
+{
+    int size;
+    std::string elem;
+    std::vector<std::string> vec, vec1;
+    std::cout << "Enter the size of vector: ";
+    std::cin >> size;
+    std::cout << "The elements of vector: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> elem;
+        vec.push_back(elem);
+    }
+    vec1 = elem_num(vec);
+    std::cout << "The result: ";
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        std::cout << vec1[i] << ' ';
+    }
+    std::cout << '\n';
+    return 0;
+}
+
+// std::cout << "Please enter the vector: ";
+//     elem_num({"Hello", "1student", "bestfrends4ever", "bye"});
+//     return 0;
+// }
